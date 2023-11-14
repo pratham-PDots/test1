f9dc541067f55c2dabf5e96e0f40d1d1a988f5c2
Pratham Jha <pratham.jha@oaralleldots.com>
2023-11-14 16:08:25 +0530

    Add images data

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NativeEventEmitter, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, FlatList, Image } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import { NativeModules } from 'react-native';
 const { CameraModule } = NativeModules;
 
+interface DataType {
+  images: { uri: string }[];
+  session_id: string;
+}
+
 function App(): JSX.Element {
+  const [events, setEvents] = useState<DataType[]>([]);
+
   const handleOpenCam = () => {
     CameraModule.openCam();
   };
+
+  useEffect(() => {
+    const eventEmitter = new NativeEventEmitter(NativeModules.CameraModule);
+    let eventListener = eventEmitter.addListener('did-receive-queue-data', (eventsData: DataType[]) => {
+      setEvents(eventsData);
+    });
+
+    // Removes the listener once unmounted
+    return () => {
+      eventListener.remove();
+    };
+  }, []);
+
+  const renderHeader = ({ item }: { item: DataType }) => (
+    <View style={styles.headerContainer}>
+      <Text style={styles.sessionId}>Session ID: {item.session_id}</Text>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {item.images.map((image, index) => (
+          <Image key={index} source={{ uri: `file://${image.uri}` }} style={styles.image} />
+        ))}
+      </View>
+    </View>
+  );
+
+  const renderImage = ({ item }: { item: DataType }) => (
+    <Image source={{ uri: `file://${item.images[0].uri}` }} style={styles.image} />
+  );
 
   return (
     <SafeAreaView style={styles.container}>
@@ -17,6 +51,21 @@ function App(): JSX.Element {
           <Text style={styles.buttonText}>Open Camera</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={events}
+        keyExtractor={(item) => item.session_id}
+        renderItem={({ item }) => (
+          <FlatList
+            ListHeaderComponent={() => renderHeader({ item })}
+            data={[item]}
+            keyExtractor={(imageItem) => imageItem.session_id}
+            renderItem={renderImage}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        )}
+      />
     </SafeAreaView>
   );
 }
@@ -40,6 +89,20 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  headerContainer: {
+    backgroundColor: Colors.lighter,
+    padding: 10,
+  },
+  sessionId: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 5,
   },
 });
 
--- android/app/src/main/java/com/test1/kotlinfiles/CameraModule.kt
+++ android/app/src/main/java/com/test1/kotlinfiles/CameraModule.kt
@@ -6,9 +6,70 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.sj.camera_lib_android.utils.CameraSDK
 import org.json.JSONObject
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.sj.camera_lib_android.Database.ReactPendingData
+import java.util.ArrayList
 
 class CameraModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "CameraModule"
+
+    private lateinit var mLocalBroadcastReceiver: LocalBroadcastReceiver
+
+
+    init {
+        mLocalBroadcastReceiver = LocalBroadcastReceiver()
+        val localBroadcastManager = LocalBroadcastManager.getInstance(reactApplicationContext)
+        localBroadcastManager.registerReceiver(
+            mLocalBroadcastReceiver,
+            IntentFilter("did-receive-queue-data")
+        )
+    }
+
+    inner class LocalBroadcastReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            if(intent.action?.equals("did-receive-queue-data") == true) {
+                val imageList = intent.getParcelableArrayListExtra<ReactPendingData>("imageList")
+                imageList?.let { images -> sendEvent("did-receive-queue-data", images) }
+            }
+        }
+    }
+
+
+    private fun sendEvent(eventName: String, params: ArrayList<ReactPendingData>) {
+        val reactContext = reactApplicationContext as ReactContext
+        val writableArray = Arguments.createArray()
+
+        for (reactPendingData in params) {
+            val writableMap = Arguments.createMap()
+            writableMap.putString("session_id", reactPendingData.session_id)
+
+            val writableArrayImages = Arguments.createArray()
+            for (reactPendingImage in reactPendingData.images) {
+                val writableMapImage = Arguments.createMap()
+                writableMapImage.putString("uri", reactPendingImage.uri)
+                writableMapImage.putBoolean("upload_status", reactPendingImage.upload_status)
+                writableMapImage.putString("error", reactPendingImage.error)
+                writableArrayImages.pushMap(writableMapImage)
+            }
+
+            writableMap.putArray("images", writableArrayImages)
+            writableArray.pushMap(writableMap)
+        }
+
+        Log.d("imageSW emit", writableArray.toString())
+
+        reactContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+            .emit(eventName, writableArray)
+    }
 
     @ReactMethod
     fun openCam() {
@@ -44,5 +105,24 @@ class CameraModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
             resolution = 2048,
             uploadFrom = "Shelfwatch"
         )
+    }
+
+    private var listenerCount = 0
+
+    @ReactMethod
+    fun addListener(eventName: String) {
+        if (listenerCount == 0) {
+            // Set up any upstream listeners or background tasks as necessary
+        }
+
+        listenerCount += 1
+    }
+
+    @ReactMethod
+    fun removeListeners(count: Int) {
+        listenerCount -= count
+        if (listenerCount == 0) {
+            // Remove upstream listeners, stop unnecessary background tasks
+        }
     }
 }
