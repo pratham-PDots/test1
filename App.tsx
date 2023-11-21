de4b284f2951856ed05de54e16b1f84c5b01fe22
Pratham Jha <pratham.jha@oaralleldots.com>
2023-11-21 18:23:50 +0530

    add params

--- App.tsx
+++ App.tsx
@@ -14,12 +14,40 @@ function App(): JSX.Element {
   const [events, setEvents] = useState<DataType[]>([]);
 
   const handleOpenCam = () => {
-    CameraModule.openCam();
+    CameraModule.openCam(
+      "portrait",
+      "80",
+      `{
+        "shop_id": 1288,
+        "project_id": "79b7c955-f253-4f19-b1ab-710585614dd4",
+        "td_version_id": 178,
+        "shelf_image_id": null,
+        "asset_image_id": null,
+        "shelf_type": "Main Aisle",
+        "category_id": 309,
+        "user_id": 133,
+        "isConnected": true,
+        "sn_image_type": "skus",
+        "image_type": "single",
+        "seq_no": 1,
+        "level": 1,
+        "last_image_flag": 1,
+        "uploadOnlyOnWifi": 0,
+        "app_session_id": "8e2faa6b-d6fe-413a-a693-76a0cbe0ce71"
+      }`,
+      true,
+      true,
+      "",
+      "2048",
+      "Shelfwatch",
+      false,
+      1.2
+    );
   };
 
   useEffect(() => {
     // Initialize the SDK when the component mounts
-    CameraModule.initSDK();
+    CameraModule.initSDK("gs://shelfwatch-app-dev");
 
     // Set up the event listener
     const eventEmitter = new NativeEventEmitter(NativeModules.CameraModule);
